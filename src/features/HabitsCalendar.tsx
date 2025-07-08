d6d93d485e20a64a9f03d623ee0373657790bbde
Mohamed-Abdellatif <mabdellatif411@gmail.com>
2025-07-08 16:17:38 +0300

    edit the calendar to show habits based on frequency

--- src/features/HabitsCalendar.tsx
+++ src/features/HabitsCalendar.tsx
@@ -1,29 +1,98 @@
 import moment from "moment";
-import { useMemo} from "react";
+import { useMemo } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import rrulePlugin from "@fullcalendar/rrule";
+import { RRule } from "rrule";
 import useHabitQuery from "@/hooks/useAddHabbit";
 
-
+const weekdayMap = [
+  RRule.SU,
+  RRule.MO,
+  RRule.TU,
+  RRule.WE,
+  RRule.TH,
+  RRule.FR,
+  RRule.SA,
+];
+interface RecurrenceOptions {
+  daysOfWeek?: number[];
+  rrule?: any;
+}
 
 const HabitsCalendar = () => {
-const { data } = useHabitQuery("habit");
+  const { data } = useHabitQuery("habit");
+
+  function getRecurrenceOptions(
+    type: string,
+    createdAt: string
+  ): RecurrenceOptions {
+    const date =
+      typeof createdAt === "string" ? new Date(createdAt) : createdAt;
+    const dayOfWeek = date.getDay();
+
+    switch (type) {
+      case "daily":
+        return {
+          daysOfWeek: [0, 1, 2, 3, 4, 5, 6],
+          rrule: {
+            freq: RRule.DAILY,
+            dtstart: createdAt,
+          },
+        };
+      case "weekly": {
+        const date = new Date(createdAt);
+        return {
+          daysOfWeek: [date.getDay()],
+          rrule: {
+            freq: RRule.WEEKLY,
+            byweekday: [weekdayMap[date.getDay()]],
+            interval: 1,
+            dtstart: date,
+          },
+        };
+      }
+      case "monthly":
+        return {
+          rrule: {
+            freq: RRule.MONTHLY,
+            interval: 1,
+            bymonthday: new Date(createdAt).getDate(),
+            dtstart: new Date(createdAt),
+          },
+        };
+      case "custom":
+        return {
+          rrule: {
+            freq: RRule.WEEKLY,
+            interval: 2,
+            byweekday: [dayOfWeek],
+            dtstart: createdAt,
+          },
+        };
+      default:
+        throw new Error("Invalid recurrence type");
+    }
+  }
 
   const events = useMemo(() => {
-    return data?.map((habit:IHabit) => ({
-      title: `${habit.name}: ${habit.completedDates?.length}`,
-      startRecur: moment(habit.createdAt).format("YYYY-MM-DD"),
-      daysOfWeek: [0, 1, 2, 3, 4, 5, 6],
-      backgroundColor: "var(--bs-primary)",
-      textColor: "BLACK",
-      borderColor: "var(--bs-primary)",
-      extendedProps: {
-        completedDates: habit.completedDates,
-        habitId: habit.name,
-      },
-    }));
+    return data?.map((habit: IHabit) => {
+      const recurrence = getRecurrenceOptions(habit.frequency, habit.createdAt);
+      return {
+        title: `${habit.name}: ${habit.completedDates?.length}`,
+        startRecur: moment(habit.createdAt).format("YYYY-MM-DD"),
+        daysOfWeek: recurrence.daysOfWeek,
+        rrule: recurrence.rrule,
+        backgroundColor: "white",
+        textColor: "BLACK",
+        borderColor: "var(--bs-primary)",
+        extendedProps: {
+          completedDates: habit.completedDates,
+          habitId: habit.name,
+        },
+      };
+    });
   }, [data]);
 
   const handleDateClick = () => {
